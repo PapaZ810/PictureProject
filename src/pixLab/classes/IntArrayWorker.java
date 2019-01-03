f90e8a51b8e23d59d018ab1278e19c1edd21c2af
Zac <zombiezac@att.net>
2019-01-03 13:26:11 -0700

    added get count method

--- src/pixLab/classes/IntArrayWorker.java
+++ src/pixLab/classes/IntArrayWorker.java
@@ -12,6 +12,24 @@ public class IntArrayWorker
     matrix = theMatrix;
   }
   
+  public int getCount(int someNumber)
+  {
+	  int count = 0;
+	  
+	  for (int [] row : matrix)
+	  {
+		  for (int item : row)
+		  {
+			  if (item == someNumber)
+			  {
+				  count++;
+			  }
+		  }
+	  }
+	  
+	  return count;
+  }
+  
   /**
    * Method to return the total 
    * @return the total of the values in the array
