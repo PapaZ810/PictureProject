941c600cbd80ae1cf1758d1be0931440da6e2bbc
Zac <zombiezac@att.net>
2019-01-11 12:38:46 -0700

    added a shift method

--- src/pixLab/classes/Picture.java
+++ src/pixLab/classes/Picture.java
@@ -246,6 +246,32 @@ public class Picture extends SimplePicture
 	  }
   }
   
+  public void shiftLeftRight(int amount) 
+  {
+	  Pixel[][] pixels = this.getPixels2D();
+	  Picture temp =new Picture(this);
+	  Pixel[][] copied = temp.getPixels2D();
+	  
+	  int shiftedValue = amount;
+	  int width = pixels[0].length;
+	  
+	  for (int row = 0; row < pixels.length; row++)
+	  {
+		  for(int col = 0; col < pixels[0].length; col++)
+		  {
+			  shiftedValue = (col + amount) % width;
+			  copied[row][col].setColor(pixels[row][shiftedValue].getColor());
+		  }
+	  }
+	  for (int row = 0; row < pixels.length; row++)
+	  {
+		  for (int col = 0; col < pixels[0].length; col++)
+		  {
+			  pixels[row][col].setColor(copied[row][col].getColor());
+		  }
+	  }
+  }
+  
   public void glitchy()
   {
 	  Pixel [][] pixels = getPixels2D();
@@ -315,7 +341,9 @@ public class Picture extends SimplePicture
   public static void main(String[] args) 
   {
     Picture beach = new Picture("beach.jpg");
-    
+    beach.explore();
+    beach.glitchy();
+    beach.explore();
   }
   
 } // this } is the end of class Picture, put all new methods before this
